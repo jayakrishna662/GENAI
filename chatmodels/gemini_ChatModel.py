c57c244a2ab206523b8bb5fc19a38c9f69e3813e
jayakrishna662 <jayakrishna7828@gmail.com>
2026-08-22 16:01:42 +0530

    Added gemini_ChatModel.py

---
+++ chatmodels/gemini_ChatModel.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model(
+    "google_genai:gemini-3.5-flash-lite",
+)
+
+response = model.invoke("What is an Embedding Model?")
+
+print(response.content[0]["text"])
