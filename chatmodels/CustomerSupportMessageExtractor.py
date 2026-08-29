0f9f9beaccfe267c0046f1d98a897ff9c3157752
jayakrishna662 <jayakrishna7828@gmail.com>
2026-08-29 14:31:07 +0530

    Updated CustomerSupportMessageExtractor.py

--- chatmodels/CustomerSupportMessageExtractor.py
+++ chatmodels/CustomerSupportMessageExtractor.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from enum import Enum
 from typing import Optional
 
 from langchain_core.output_parsers import PydanticOutputParser
@@ -17,6 +18,14 @@ model = ChatGroq(
 )
 
 
+
+# The sentiment must be one of the values defined in Sentiment.
+class Sentiment(str, Enum):
+    POSITIVE = "Positive"
+    NEUTRAL = "Neutral"
+    NEGATIVE = "Negative"
+
+
 # Define the Ouput structure we want
 # Create SupportTicket class using Pydantic's BaseModel features.
 # BaseModel is the parent class provided by Pydantic. It is a tool for data validation.
@@ -26,11 +35,10 @@ class SupportTicket(BaseModel):
     customer_name: Optional[str] = None  # Optional[str] means this field can be a string or None.
     order_id: Optional[str] = None  
     product: str
-    issue_type: str
-    issue_description: str
+    issue_type: Optional[str] = None 
+    issue_description: Optional[str] = None 
     requested_action: Optional[str] = None  
-    sentiment: str
-
+    sentiment: Sentiment
 
 # Create Pydantic parser
 # We are telling the parser to take the AI's response and convert it into a SupportTicket object.None
@@ -51,6 +59,8 @@ prompt = ChatPromptTemplate.from_messages([
         - Only extract information that is explicitly stated in the customer message.
         - Do NOT infer, assume, or guess missing information.
         - If information is not mentioned, return None.
+        - For issue_type, classify the customer's problem into a concise category such as:
+            Damaged Product, Technical Issue, Billing Error, Delivery Delay, Wrong Item, etc.
         {format_instructions} # This is a placeholder for parser.get_format_instructions(), that generates instructions telling the AI how its output should be formatted according to your SupportTicket model.
         """
     ),
@@ -83,4 +93,5 @@ ticket = parser.parse(response.content)
 
 
 # Print result
-print(ticket)
+output=ticket.model_dump_json() # converts it into a JSON string: since ticket  is a Pydantic object.
+print(output)
