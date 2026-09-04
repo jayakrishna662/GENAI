d62c30775ece4dc3ff0c11687a243bda2f867a13
jayakrishna662 <jayakrishna7828@gmail.com>
2026-09-04 20:35:19 +0530

    Added textLoader.py

---
+++ DocumentLoaders/textLoader.py
@@ -0,0 +1,15 @@
+from langchain_community.document_loaders import TextLoader
+
+# Create a TextLoader pointing to the file path
+# encoding="utf-8" ensures special characters (emojis, symbols, non-English text) 
+# are read correctly — without it, Windows may default to 'cp1252' encoding 
+# and throw a UnicodeDecodeError on such characters
+loader = TextLoader("C:\\Users\\Jaykr\\Desktop\\test1.txt",  encoding="utf-8")
+
+documents = loader.load() # read data source and convert into LangChain document format
+
+print(documents) # Prints the entire list, including the Document object wrapper itself.
+
+print(documents[0].page_content) # from first Document object, print only the text content from that object
+
+print(documents[0].metadata) # from first Document object, print only the metadata as a dictionary from that object
